00347544e010c7801ebab52df44a649116ac67cf
ayush-bahuguna <ayushbahuguna7@gmail.com>
2019-08-15 01:26:20 +0530

    Update userCamera.js

--- userCamera.js
+++ userCamera.js
@@ -1,14 +1,20 @@
 (function(){
-	var video = document.getElementById('video') ;
+	video = document.getElementById('video') ;
 	canvas = document.getElementById('canvas') ;
 	context = canvas.getContext('2d') ;
+	
+	// photo is used so that the image can be obtained on the canvas and we can work on it.
 	photo = document.getElementById('photo') ;
+	
 	vendorURL = window.URL ||
-				window.webkitURL ;
+		    window.webkitURL ;
+	
+	//for compatibility with all browsers
 	navigator.getMedia = navigator.getUserMedia ||
-						 navigator.webkitGetUserMedia ||
-						 navigator.mozGetUserMedia ||
-						 navigator.msGetUserMedia ;
+			     navigator.webkitGetUserMedia ||
+			     navigator.mozGetUserMedia ||
+			     navigator.msGetUserMedia ;
+	//for getting image
 	navigator.getMedia({
 		video:true,
 		audio:false
@@ -19,11 +25,11 @@
 		//error.code
 	});
 
-
+	//for capturing image 
 	document.getElementById('cap-btn').addEventListener('click', function(){
 		context.drawImage(video, 0, 0, 400, 300)
+		
+		//set image source as the snap from camera
 		photo.setAttribute('src', canvas.toDataURL('image'))
 	});
-
-
 })();
