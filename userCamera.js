8e4b318b3c031d713837c9ed45ea7ce0c55c5899
ayush-bahuguna <ayushbahuguna7@gmail.com>
2019-08-15 20:23:08 +0530

    (fixed) : Open back camera of device

--- userCamera.js
+++ userCamera.js
@@ -1,35 +1,52 @@
 (function(){
-	video = document.getElementById('video') ;
+
+	//for camera video stream
+	var video = document.getElementById('video') ;
+	
+	//for showing camera stream on page
 	canvas = document.getElementById('canvas') ;
 	context = canvas.getContext('2d') ;
-	
-	// photo is used so that the image can be obtained on the canvas and we can work on it.
+
+	//for getting camera stream as a still picture
 	photo = document.getElementById('photo') ;
-	
+
+	//for getting number of input options
+	const select = document.getElementById('select');
+
+
+	function gotDevices(mediaDevices) {
+    select.innerHTML = '';
+  	select.appendChild(document.createElement('option'));
+  	let count = 1;
+  	mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  	});
+	}
+
+	//for showing media stream, compatible with most of the browsers
 	vendorURL = window.URL ||
-		    window.webkitURL ;
-	
-	//for compatibility with all browsers
+				window.webkitURL ;
 	navigator.getMedia = navigator.getUserMedia ||
-			     navigator.webkitGetUserMedia ||
-			     navigator.mozGetUserMedia ||
-			     navigator.msGetUserMedia ;
-	//for getting image
-	navigator.getMedia({
-		video:true,
-		audio:false
-	}, function(stream) {
+						 navigator.webkitGetUserMedia ||
+						 navigator.mozGetUserMedia ||
+						 navigator.msGetUserMedia ;
+	navigator.getMedia({video: { facingMode: "environment" } }, function(stream) {
 		video.srcObject = stream ;
 		video.play() ;
 	}, function(error){
-		//error.code
+		alert("Error: Permission denied or camera could not be opened")
 	});
 
-	//for capturing image 
+	//drawing image on canvas which can be used further
 	document.getElementById('cap-btn').addEventListener('click', function(){
 		context.drawImage(video, 0, 0, 400, 300)
-		
-		//set image source as the snap from camera
 		photo.setAttribute('src', canvas.toDataURL('image'))
 	});
 })();
